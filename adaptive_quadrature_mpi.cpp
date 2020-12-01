1ee82ab3d140934879cb1a75b0606fb9ec61630d
initrail <oneofuser25@gmail.com>
2020-12-01 14:56:06 -0800

    Started implementing adaptive quadrature using MPI.

--- adaptive_quadrature_mpi.cpp
+++ adaptive_quadrature_mpi.cpp
@@ -8,16 +8,22 @@ float area = 0;
 MPI_Win idlewin;
 MPI_Win areawin;
 int P;
-void rmaOperation(void* var1, MPI_Op op, void* var2, MPI_Datatype type,  MPI_Win& window){	
+static const int ARRAY_LEN = 1000;
+struct task{
+	float a;
+	float b;
+};
+
+void rmaOperation(void* var1, MPI_Op op, void* var2, MPI_Datatype type,  MPI_Win& window/*, int rank*/){	
 	MPI_Win_lock(MPI_LOCK_EXCLUSIVE, 0, 0, window);
 	MPI_Fetch_and_op(var2, var1, type, 0, 0, op, window);
-	MPI_Get(var1, 1, type, 0, 0, 1, type, window);
+	//MPI_Get(var1, 1, type, 0, 0, 1, type, window);
 	MPI_Win_unlock(0, window);
 	//MPI_Get and the print statement are for debugging purposes
-	if(type == MPI_INT)
-		printf("idle = %d\n", *((int*)var1));
+	/*if(type == MPI_INT)
+		printf("p %d idle = %d\n", rank, *((int*)var1));
 	else if(type == MPI_FLOAT)
-		printf("area = %f\n", *((float*)var1));
+		printf("p %d area = %f\n", rank, *((float*)var1));*/
 }
 
 void* rmaGet(void* var, MPI_Datatype type, MPI_Win& window){
@@ -30,15 +36,8 @@ void* rmaGet(void* var, MPI_Datatype type, MPI_Win& window){
 void adaptiveQuadrature(float* range, float tolerance, int rank){
 	int decr = -1;
 	int incr = 1;
-	float area1 = 0;
-	MPI_Request req;
-	rmaOperation(&idle, MPI_SUM, &decr, MPI_INT, idlewin);
-	idle = *((int*) rmaGet(&idle, MPI_INT, idlewin));
-	//printf("process %d idle = %d\n", rank, idle);
-	rmaOperation(&area, MPI_SUM, &area1, MPI_FLOAT, areawin);
-	area = *((float*) rmaGet(&area, MPI_FLOAT, areawin));
-	//printf("process %d area = %f\n", rank, area);
-	while(true){
+	float area1 = 0.0;
+	/*while(true){
 		idle = *((int*)rmaGet(&idle, MPI_INT, idlewin));
 		if(workready() || (idle != P))
 			//idle = idle - 1
@@ -50,16 +49,25 @@ void adaptiveQuadrature(float* range, float tolerance, int rank){
 		}
 		//idle = idle + 1
 		rmaOperation(&idle, MPI_SUM, &incr, MPI_INT, idlewin);
-	}
+	}*/
 }
 
 int main(int argc, char** argv){
 	int rank;
 	float* range;
 	float tolerance;
+	task tasks[ARRAY_LEN];
+	MPI_Datatype taskType;
+	int blocklen[2] = {1, 1};
+	MPI_Aint indices[2];
+	indices[0] = (MPI_Aint)&tasks[0].a - (MPI_Aint)tasks;
+	indices[1] = (MPI_Aint)&tasks[0].b - (MPI_Aint)tasks;
+	int types[3] = {MPI_FLOAT, MPI_FLOAT};
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &P);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Type_create_struct(2, blocklen, indices, types, &taskType);
+	MPI_Type_commit(&taskType);
 	idle = P;
 	if(rank == 0){
 		MPI_Win_create(&idle, sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &idlewin);
